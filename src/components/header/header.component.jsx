ece64dbe4455fe973533ca60a70cc232a2a7d069
Patryk Uran <patryk.uran@dcx.bz>
2020-01-08 11:33:37 +0100

    implement user reducer and user actions

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from "react-redux";
 
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 import {auth} from "../../firebase/firebase.utils";
@@ -7,24 +8,32 @@ import {auth} from "../../firebase/firebase.utils";
 import './header.styles.scss';
 
 const Header = ({currentUser}) => (
-    <div className='header'>
-      <Link className='logo-container' to='/'>
-        <Logo className='logo'/>
+  <div className='header'>
+    {console.log(currentUser)}
+    <Link className='logo-container' to='/'>
+      <Logo className='logo'/>
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
       </Link>
-      <div className='options'>
-        <Link className='option' to='/shop'>
-          SHOP
-        </Link>
-        <Link className='option' to='/shop'>
-          CONTACT
-        </Link>
-        {currentUser ?
-            <div className='option' onClick={() => {auth.signOut()}}>SIGN OUT</div>
-            :
-            <Link className='option' to='/signin'>SIGN IN</Link>
-        }
-      </div>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
+      {currentUser ?
+        <div className='option' onClick={() => {
+          auth.signOut()
+        }}>SIGN OUT</div>
+        :
+        <Link className='option' to='/signin'>SIGN IN</Link>
+      }
     </div>
+  </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+
+export default connect(mapStateToProps)(Header);
