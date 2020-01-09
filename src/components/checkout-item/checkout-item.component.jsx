7c7892e4975c672f06590a04cd6bc4f59cbe63b1
Patryk Uran <patryk.uran@dcx.bz>
2020-01-09 13:40:10 +0100

    remove item from cart on click

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import {clearItemFromCart} from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem }) => {
+const CheckoutItem = ({ cartItem, clearItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
   return (
     <div className='checkout-item'>
@@ -13,11 +14,15 @@ const CheckoutItem = ({ cartItem }) => {
       <span className='name'>{name}</span>
       <span className='quantity'>{quantity}</span>
       <span className='price'>{price}</span>
-      <div className='remove-button'>
+      <div className='remove-button' onClick={() => clearItem(cartItem)}>
         &#10005;
       </div>
     </div>
   );
 };
 
-export default connect()(CheckoutItem);
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(clearItemFromCart(item))
+})
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -27,7 +27,7 @@ const CheckoutPage = ({cartItems, cartTotal}) => (
         <span>Remove</span>
       </div>
     </div>
-    {cartItems.map(cartItem => <CheckoutItem id={cartItem.id} cartItem={cartItem}/>)}
+    {cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem}/>)}
     <div className='total'>TOTAL: ${cartTotal}</div>
   </div>
 );
