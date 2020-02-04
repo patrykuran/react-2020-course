8d985b382c6cb4e299567654ba0ad44fcdefbcd6
Patryk Uran <patryk.uran@dcx.bz>
2020-02-04 10:06:53 +0100

    remove unused code

--- src/App.js
+++ src/App.js
@@ -12,8 +12,6 @@ import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
@@ -22,21 +20,6 @@ class App extends React.Component {
 
   componentDidMount() {
     const { setCurrentUser } = this.props;
-
-    // this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-    //   if (userAuth) {
-    //     const userRef = await createUserProfileDocument(userAuth);
-    //
-    //     userRef.onSnapshot(snapShot => {
-    //       setCurrentUser({
-    //         id: snapShot.id,
-    //         ...snapShot.data()
-    //       });
-    //     });
-    //   }
-    //
-    //   setCurrentUser(userAuth);
-    // });
   }
 
   componentWillUnmount() {
